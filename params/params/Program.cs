b5838aac08195f708b852f8d075f93e301bfda9a
Haydar <127090984+HaydarAIi@users.noreply.github.com>
2023-03-06 13:25:06 +0300

    Add files via upload
    Here, params is being explained in C# language.

---
+++ params/params/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(plus(9564,6785,4455));
+
+            Console.WriteLine(multiple(4,5,6,7,8,9));
+            Console.ReadKey(true);
+
+
+
+        }
+        static int plus(params int[] sayilar)
+        {
+            //It is used to pass a variable number of arguments (parameters) to a method.
+            int toplam = 0;
+            foreach(var item in sayilar)
+            {
+                toplam += item;
+            }
+            return toplam;
+        }
+        static int multiple(params int[] nums)
+        {
+            int result = 1;
+            foreach(var item in nums)
+            {
+                result = result * item;
+            }
+            return result;  
+        }
+    }
+}
