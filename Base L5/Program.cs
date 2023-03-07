b26c421d88faf2048e6e50bda775c4407b8d6a5a
Hytrar <127090984+HaydarAIi@users.noreply.github.com>
2023-03-07 19:05:01 +0300

    Add files via upload
    base explained

---
+++ Base L5/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_L5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           BaseClass abc = new BaseClass();
+           DerivedClass derived = new DerivedClass();
+            Console.ReadKey();
+
+        }
+    }
+    class BaseClass
+    {
+        public BaseClass()
+        {
+            Console.WriteLine("Base class constructor.");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass() : base()
+        {
+            Console.WriteLine("Derived class constructor.");
+        }
+    }
+}
