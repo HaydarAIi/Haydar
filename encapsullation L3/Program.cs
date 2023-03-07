caea01fc245285247e3fd841bdd9c7e05b3434c8
Hytrar <127090984+HaydarAIi@users.noreply.github.com>
2023-03-07 12:59:06 +0300

    Add files via upload
    Encapsullation

---
+++ encapsullation L3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsullation_L3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Name = "Citroen";
+            car.Model = "C5";
+            car.year = 2008;
+        }
+    }
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+          
+        public int year { get; set; }   
+
+    }
+}
