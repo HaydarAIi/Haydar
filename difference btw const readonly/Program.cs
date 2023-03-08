03019a91a5c74c30e7f3f625b63b4ca14ad01cd4
Hytrar <127090984+HaydarAIi@users.noreply.github.com>
2023-03-08 17:33:20 +0300

    difference between const and readonly

---
+++ difference btw const readonly/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace difference_btw_const_readonly_L7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int constValue = 5;
+            Console.WriteLine("constValue: {0}", constValue);//you have to give value at first
+
+            MyClass myClass = new MyClass();//you can give a value whenever you want
+            Console.WriteLine("myClass.ReadOnlyValue: {0}", myClass.ReadOnlyValue);
+        }
+        class MyClass
+        {
+            public readonly int ReadOnlyValue;
+
+            public MyClass()
+            {
+                ReadOnlyValue = 10;
+            }
+        }
+    }
+}
